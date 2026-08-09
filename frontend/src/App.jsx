4dd3555fefde34ebda4c84bb91676d0c64179b0d
uchimakali-dev <uchimakali.dev@gmail.com>
2026-08-09 14:40:27 +0530

    FIX api irl

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,7 @@ export default function App() {
   const [students, setStudents] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const API_BASE_URL = "http://127.0.0.1:8000/api";
+  const API_BASE_URL = "/api";
   const [token,setToken]=useState(localStorage.getItem('token'))
 
   const isTokenExpired = (token) => {
