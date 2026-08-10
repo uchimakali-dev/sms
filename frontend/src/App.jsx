e7aaf415537fa364c87461a42d06e951e7027945
uchimakali-dev <uchimakali.dev@gmail.com>
2026-08-10 15:14:20 +0530

    home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -219,7 +219,7 @@ export default function App() {
                 </ProtectedRoute>
               }
             />
-            <Route path="*" element={<Navigate to="/login" replace />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
       </div>
